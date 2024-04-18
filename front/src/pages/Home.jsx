689b9f0666042a72324c5b2e862680a9eedfa853
Douglas <douglasvitor1233@gmail.com>
2024-04-18 13:16:35 -0300

    building the home page

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,12 +1,16 @@
+import LinkButton from '../components/layouts/LinkButton'
+import savings from '../img/savings.svg'
+import styles from './Home.module.css'
 
 
 function Home(){
     return (
         <>
-        <section>
+        <section className={styles.home_container}>
             <h1>Bem vindo ao <span>Costs</span></h1>
             <p>Comece a gerenciar os seus projetos agora mesmo!</p>
-            <a href="/">Criar projeto</a>
+            <LinkButton to="/novoprojeto" text="Criar Projeto"/>
+            <img src={savings} alt="Costs" />
         </section>
         </>
     )
