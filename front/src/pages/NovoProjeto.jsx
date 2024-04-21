4af8d70a7f513f5a04980cf9348c720e0e7db692
douglasvcv <douglasvitor1233@gmail.com>
2024-04-21 18:57:53 -0300

    building projectForm

--- front/src/pages/NovoProjeto.jsx
+++ front/src/pages/NovoProjeto.jsx
@@ -1,3 +1,4 @@
+import ProjectForm from '../components/projects/ProjectForm'
 import styles from './NovoProjeto.module.css'
 
 function NovoProjeto(){
@@ -5,7 +6,7 @@ function NovoProjeto(){
         <div className={styles.novoProjeto_container}>
         <h1>Criar projeto</h1>
         <p>Crie seu projeto para depois adicionar os serviços</p>
-        <p>formulário</p>
+        <ProjectForm/>
         </div>
     )
 }
